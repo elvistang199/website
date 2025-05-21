4c4ee0a5d20355462fe0b35491947ffef0c3dad3
elvistang199 <70172078+elvistang199@users.noreply.github.com>
2025-05-21 12:32:20 -0700

    Delete script3.js
    removed script3

--- script3.js
+++
@@ -1,9 +0,0 @@
-// Get references to the button and the hidden image
-const revealButton2 = document.getElementById('revealButton2');
-const hiddenImage = document.getElementById('hiddenImage');
-
-// Add an event listener to the button
-revealButton2.addEventListener('click', () => {
-   // Show the hidden image
-   hiddenImage.classList.remove('hidden');
-});
